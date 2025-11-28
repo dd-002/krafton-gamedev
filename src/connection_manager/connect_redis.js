553997a924ec5efc556a8f0ecd7e1da394010360
Dipayan Das <d.dipayan2002@gmail.com>
2025-11-28 16:10:52 +0000

    Variable renaming

--- src/connection_manager/connect_redis.js
+++ src/connection_manager/connect_redis.js
@@ -1,18 +1,18 @@
 import { createClient } from 'redis';
 
-const client = createClient({
+const redisClient = createClient({
         host: '127.0.0.1', 
         port: process.env.REDIS_PORT,   
 });
-client.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
 async function connectToDatabase() {
   try {
-    await client.connect(); // Await the connection
+    await redisClient.connect(); // Await the connection
     console.log('Connected to Redis successfully!');
   } catch (err) {
     console.error('Failed to connect to Redis:', err);
   }
 }
 
-module.exports = {client, connectToDatabase}
+module.exports = {redisClient, connectToDatabase}
